ec606b2bd9aafc198d97d0d038b3a8d833e1baf4
Jann <janncarlomoreno@gmail.com>
2019-04-19 18:23:32 -0500

    Initial commit

---
+++ analizador_lexico.py
@@ -0,0 +1,22 @@
+full_path= ['/home/jann/Documents/UN/2019-1/Lenguajes/analizadorLexico/prueba.txt', 'tu/path/hacia/el/archivo']
+
+'''
+f reads the .txt file with the code
+'''
+f = open(full_path[0], 'r')
+
+
+for x in f:
+    '''
+    In this For loop we're going to execute our lexer logic.
+
+    x => of type string, is the current line on the file.
+    Some usefull code snippets are:
+    len(x)
+    x[n]
+    etc
+    '''
+
+
+
+
